9798530d405dbc577c90fd383bd6a2234503fa78
Ben Petito <benpetito@gmail.com>
2021-12-23 14:14:19 +1030

    Add growl when delivery checkbox is changed

--- src/main/java/modules/tableRowConditionTest/Invoice/InvoiceBizlet.java
+++ src/main/java/modules/tableRowConditionTest/Invoice/InvoiceBizlet.java
@@ -1,10 +1,12 @@
 package modules.tableRowConditionTest.Invoice;
 
+import org.skyve.domain.messages.MessageSeverity;
 import org.skyve.metadata.model.document.Bizlet;
 import org.skyve.util.Util;
 import org.skyve.web.WebContext;
 
 import modules.tableRowConditionTest.domain.Invoice;
+import modules.tableRowConditionTest.domain.InvoiceItem;
 
 public class InvoiceBizlet extends Bizlet<Invoice> {
 
@@ -13,6 +15,11 @@ public class InvoiceBizlet extends Bizlet<Invoice> {
 	@Override
 	public void preRerender(String source, Invoice bean, WebContext webContext) throws Exception {
 		Util.LOGGER.info("InvoiceBizlet:preRererender(" + source + ")");
+
+		if (InvoiceItem.deliveryPropertyName.equals(source)) {
+			webContext.growl(MessageSeverity.info, "Delivery was changed");
+		}
+
 		super.preRerender(source, bean, webContext);
 	}
 }
